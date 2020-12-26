a9cfe67ba8f7f6a3185858f21c9163796d905c8d
rimsha7 <72002316+rimsha7@users.noreply.github.com>
2020-12-27 01:57:06 +0500

    blog and places
    blog and places

--- routes/api/place.js
+++ routes/api/place.js
@@ -8,7 +8,7 @@ const multer = require("multer");
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "C:/Users/sidra/Desktop/Backend/travel/public/images/hotels");
+    cb(null, "C:Users/DeLL/Documents/GitHub/TravelFantasies/public/images/places");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
@@ -40,26 +40,15 @@ const upload = multer({
 /* GET users listing. */
 router.get("/", async (req, res) => {
   //res.send(["Pen", "Pencil"]);
-  let page = Number(req.query.page ? req.query.page : 1);
-  let perPage = Number(req.query.perPage ? req.query.perPage : 20);
+  let page = Number(req.query.page ? req.query.page : 2);
+  let perPage = Number(req.query.perPage ? req.query.perPage : 30);
   let skipRecords = perPage * (page - 1);
   let places = await Place.find().skip(skipRecords).limit(perPage);
   //let places = await Place.find();
   return res.send(places);
 });
 
-// /* GET single user . */
-// router.get("/:id", async (req, res) => {
-//   //res.send(["Pen", "Pencil"]);
-//   try {
-//     let user = await User.findById(req.params.id);
-//     if (!user)
-//       return res.status(400).send("User with given ID is not present ");
-//     return res.send(user);
-//   } catch (err) {
-//     return res.status(400).send("Invalid ID");
-//   }
-// });
+
 // /* Update Record */
 router.put("/:id", upload.single("file"), async (req, res) => {
   let place = await Place.findById(req.params.id);
@@ -72,11 +61,6 @@ router.put("/:id", upload.single("file"), async (req, res) => {
   return res.send(place);
 });
 
-// /* Delete Record */
-// router.delete("/:id", async (req, res) => {
-//   let user = await User.findByIdAndDelete(req.params.id);
-//   return res.send(user);
-// });
 
 /* Insert Record */
 router.post("/", upload.single("file"), async (req, res) => {
